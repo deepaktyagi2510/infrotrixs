cd34d96ecd5f2195a540a51275c715a45f0d2e5a
deepaktyagi2510 <138960024+deepaktyagi2510@users.noreply.github.com>
2023-07-08 11:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("you must write some things");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+
+
+
+function saveData(){
+
+   localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask()
+function search(){
+    
+}
